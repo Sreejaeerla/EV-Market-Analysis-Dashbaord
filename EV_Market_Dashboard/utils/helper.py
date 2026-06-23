8ac4e2eda643977bb6b4e749aced16357aa52f0b
Sreejaeerla <eerlasreeja97@gmail.com>
2026-06-23 17:10:34 +0530

    Update helper.py

--- EV_Market_Dashboard/utils/helper.py
+++ EV_Market_Dashboard/utils/helper.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import streamlit as st
+import os
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("EV_MARKET_DASHBOARD"/"1.data/cleaned_EV_Dataset3.xls")
+    # Works both locally and on Streamlit Cloud
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    file_path = os.path.join(base_dir, "1.data", "cleaned_EV_Dataset3.xls")
+    df = pd.read_csv(file_path)
     return df
